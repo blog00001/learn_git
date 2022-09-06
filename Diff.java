11c40cd11714003128c16f77114b4d1c6909743d
shubham hiwale <shubhamhiwale13@gmail.com>
2022-09-06 12:49:16 +0530

    Implimenetaion of diff

--- Diff.java
+++ Diff.java
@@ -1,5 +1,7 @@
 
 
 public class Diff {
+   public static void main(String[] args) {
     
+   } 
 }
---
+++ Multiply.java
@@ -0,0 +1,12 @@
+package learngit;
+
+public class Multiply {
+   
+        public static void main(String[] args)
+        {
+            int a =5;
+            int b =10;
+            System.out.println("Product is"+a*b);
+        } 
+    }
+
